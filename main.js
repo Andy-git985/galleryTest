348dcc90f5f46100e1fcec720d6233ab183d8ee8
Andy-git985 <andy.yu617@gmail.com>
2022-06-18 22:20:17 -0400

    added fade to prev and next

--- main.js
+++ main.js
@@ -3,6 +3,21 @@ const thumbBar = document.querySelector('.thumb-bar');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
+const timing = {
+  duration: 1500,
+};
+
+const fade = [
+  {
+    // from
+    opacity: 0.4,
+  },
+  {
+    // to
+    opacity: 1,
+  },
+];
+
 /* Declaring the array of image filenames */
 const images = [
   'images/pic1.jpg',
@@ -24,11 +39,13 @@ thumbBar.addEventListener('click', (e) => {
 
 prev.addEventListener('click', () => {
   const currentSlide = displayedImage.attributes.src.value;
+  displayedImage.animate(fade, timing);
   displayedImage.src = images.at(images.indexOf(currentSlide) - 1);
 });
 
 next.addEventListener('click', () => {
   const currentSlide = displayedImage.attributes.src.value;
+  displayedImage.animate(fade, timing);
   // handles undefined when on end of array
   displayedImage.src = images.at(images.indexOf(currentSlide) + 1) || images[0];
 });
